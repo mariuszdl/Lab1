15d40c43c8adbfa568d8ae9abd4494520ce604a9
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-03-07 22:21:04 +0100

    Tabelka logarytmów o podstawie 2

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -56,5 +56,19 @@ public class Main {
         }
 
         // ----------------------------------------------------------------
+        System.out.println("\nTabelka logarytmów o podstawie 2 oraz akumulowana suma kolejnych poteg liczby 2:");
+        System.out.printf("\narg\t\tlog2(arg)\tsum(arg)\n");
+
+        int arg = 1, sum = 0;
+        double log;
+
+        do {
+            log = Math.log(arg) / Math.log(2);
+            sum += arg;
+            System.out.printf("%d\t\t\t%.0f\t\t\t%d\n", arg, log, sum);
+            arg *= 2;
+        } while (arg < 20);
+
+        // ----------------------------------------------------------------
     }
 }
